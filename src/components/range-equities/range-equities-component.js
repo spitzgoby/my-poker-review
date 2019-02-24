a45be5ba899c92d8a59d4174d67d0f63077f18af
Thomas Leu <tomleu@gmail.com>
2019-02-24 17:59:02 -0600

    Add styles and minimal text to equities

--- src/components/range-equities/range-equities-component.js
+++ src/components/range-equities/range-equities-component.js
@@ -1,8 +1,38 @@
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
 import React, {Component} from 'react'
 
+import "components/range-equities/range-equities.scss"
+
 class RangeEquities extends Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+    equities: PropTypes.arrayOf(PropTypes.string)
+  }
+
+  static defaultProps = {
+    equities: [.5, .5]
+  }
+
   render() {
-    return null
+    return (
+      <div className={this.getClass()}>
+        {this.renderEquities()}
+      </div>
+    )
+  }
+
+  renderEquities() {
+    return this.props.equities.map((equity) => {
+      return <div className="range-equities--equity">
+        {equity * 100 + '%'}
+      </div>
+    })
+  }
+
+  getClass() {
+    return classnames('range-equities', this.props.className)
   }
 }
 
