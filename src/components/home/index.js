047bb5ab343000ee10ecfcc49c529e9e60ef9fb0
Thomas Leu <tomleu@gmail.com>
2019-02-24 16:41:12 -0600

    Add equity output and fix spacing

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -11,9 +11,37 @@ class Home extends Component {
       <div className="home">
         <RangeBuilder className="home--range-builder" />
         <div className="home--range-info"> 
-          <RangeOutput />
-          <RangeInput />
+          {this.renderRangeOuput()}
+          {this.renderRangeInput()}
+          {this.renderRangeEquities()}
         </div>
+      </div>
+    )
+  }
+
+  renderRangeOuput() {
+    return (
+      <div>
+        <h2>Opponent's Range</h2>
+        <RangeOutput />
+      </div>
+    )
+  }
+
+  renderRangeInput() {
+    return (
+      <div className="home--range-input">
+        <h2>My Hand</h2>
+        <RangeInput />
+      </div>
+    )
+  }
+
+  renderRangeEquities() {
+    return (
+      <div className="home--range-equities">
+        <h2>Equity</h2>
+        TODO: build this
       </div>
     )
   }
--- src/components/range-input/range-input-component.js
+++ src/components/range-input/range-input-component.js
@@ -1,14 +1,25 @@
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
 import React, {Component} from 'react'
 
 import 'components/range-input/range-input.scss'
 
 class RangeInput extends Component {
+
+  static propTypes = {
+    className: PropTypes.string
+  }
+
   render() {
     return (
-      <div className="range-input">
+      <div className={this.getClass()}>
         <input></input>
       </div>
     )
+  }
+
+  getClass() {
+    return classnames('range-input', this.props.className)
   }
 }
 
