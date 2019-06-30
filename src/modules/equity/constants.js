e83000d80a3f90cfb79d612c2882d0c973760ece
Thomas Leu <tomleu@gmail.com>
2019-06-29 20:27:01 -0500

    Clear equity when cards change

--- src/modules/equity/constants.js
+++ src/modules/equity/constants.js
@@ -2,3 +2,9 @@ export const types = {
   CALCULATE_EQUITY_INIT: '@my-poker-review/range-builder/CALCULATE_EQUITY_INIT',
   CALCULATE_EQUITY_SUCCESS: '@my-poker-review/range-builder/CALCULATE_EQUITY_SUCCESS',
 }
+
+export const EMPTY_EQUITY = {
+  lose: null,
+  tie: null,
+  win: null
+}
--- src/modules/equity/reducers/equity.js
+++ src/modules/equity/reducers/equity.js
@@ -1,4 +1,8 @@
-import {types} from 'modules/equity/constants'
+import {
+  EMPTY_EQUITY,
+  types
+} from 'modules/equity/constants'
+import {types as rangeBuilderTypes} from 'modules/range-builder/constants'
 
 const initialState = {
   equities: {},
@@ -19,12 +23,46 @@ export default (state = initialState, action = {}) => {
       }
       break
 
+    case rangeBuilderTypes.CLEAR_ALL:
+    case rangeBuilderTypes.CLEAR_ALL_SELECTED_COMBOS:
+    case rangeBuilderTypes.CLEAR_BOARD:
+    case rangeBuilderTypes.CLEAR_HAND:
+    case rangeBuilderTypes.SELECT_BOARD_CARDS:
+    case rangeBuilderTypes.SELECT_COMBOS:
+    case rangeBuilderTypes.SELECT_HAND_CARDS:
+    case rangeBuilderTypes.SET_BOARD:
+    case rangeBuilderTypes.SET_HAND:
+      nextState = {
+        ...state,
+        equities: {},
+        pending: {}
+      }
+      break
+
+    case rangeBuilderTypes.CLEAR_SELECTED_COMBOS_FROM_RANGE:
+    case rangeBuilderTypes.DELETE_RANGE:
+      nextState = {
+        ...state,
+        equities: {
+          ...state.equities,
+          [action.payload.id]: {...EMPTY_EQUITY}
+        },
+        pending: {
+          ...state.equities,
+          [action.payload.id]: false
+        }
+      }
+      break
+
+
     case types.CALCULATE_EQUITY_SUCCESS:
       nextState = {
         ...state,
         equities: {
           ...state.equities,
-          [action.payload.rangeId]: action.payload.results
+          [action.payload.rangeId]: state.pending[action.payload.rangeId] 
+            ? action.payload.results
+            : {}
         },
         pending: {
           ...state.pending,
