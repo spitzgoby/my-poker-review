321e19776193e827b18b6859562f84d74c834ec2
Thomas Leu <tomleu@gmail.com>
2019-07-09 19:21:54 -0500

    Persist changes in session storage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'connected-react-router'
-import store, { history } from './store'
 import App from 'components/app'
+import {ConnectedRouter} from 'connected-react-router'
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/lib/integration/react'
+import store, { 
+  history,
+  persistor
+} from './store'
 
 import 'sanitize.css/sanitize.css'
 import './index.css'
@@ -14,7 +18,9 @@ render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <App />
+        <PersistGate loading={<span>loading</span>} persistor={persistor}>
+          <App />
+        </PersistGate>
       </div>
     </ConnectedRouter>
   </Provider>,
--- src/store.js
+++ src/store.js
@@ -1,14 +1,21 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { connectRouter, routerMiddleware } from 'connected-react-router'
-import thunk from 'redux-thunk'
+import {connectRouter, routerMiddleware} from 'connected-react-router'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './modules'
+import {createStore, applyMiddleware, compose} from 'redux'
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage/session'
+import thunk from 'redux-thunk'
 
 export const history = createHistory()
 
 const initialState = {}
 const enhancers = []
 const middleware = [thunk, routerMiddleware(history)]
+const persistConfig = {
+  key: 'my-poker-review',
+  storage: storage
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
@@ -23,8 +30,11 @@ const composedEnhancers = compose(
   ...enhancers
 )
 
-export default createStore(
-  connectRouter(history)(rootReducer),
+export const store = createStore(
+  connectRouter(history)(persistedReducer),
   initialState,
   composedEnhancers
 )
+export const persistor = persistStore(store)
+
+export default store
